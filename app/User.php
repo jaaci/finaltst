514a605018d948ed5b685981e4087bae54a83a35
Arturo Castillo <jaaci@live.com>
2018-05-13 23:10:12 -0500

    Mongo DB
    Version con mongo DB, mi ambiente de desarrollo por algun motivo fallaba

---
+++ app/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+
+class User extends Eloquent implements Authenticatable {
+    use AuthenticableTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
